40e822422fa8c2346b955ba02070c044bba87d8b
lucascampos42 <bhlucascampos@gmail.com>
2025-08-28 17:59:29 -0300

    🧪 test: adapted tests to cover additional scenarios

--- lib/ui/widgets/search_bar_widget.dart
+++ lib/ui/widgets/search_bar_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import '../../core/utils/constants.dart';
+import '../../core/theme/app_theme.dart';
+import '../../core/utils/app_constants.dart';
+import '../../core/utils/responsive_helper.dart';
 
 class SearchBarWidget extends StatefulWidget {
   final TextEditingController controller;
@@ -34,42 +36,64 @@ class _SearchBarWidgetState extends State<SearchBarWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(AppConstants.defaultPadding),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey[100],
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black,
-              spreadRadius: 1,
-              blurRadius: 4,
-              offset: const Offset(0, 2),
+    final isDesktop = ResponsiveHelper.isDesktop(context);
+    final maxWidth = isDesktop ? 600.0 : double.infinity;
+    
+    return ResponsiveContainer(
+      padding: ResponsiveHelper.getResponsivePadding(context),
+      child: Center(
+        child: Container(
+          width: double.infinity,
+          constraints: BoxConstraints(maxWidth: maxWidth),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(isDesktop ? 16 : 12),
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(20, 108, 108, 108),
+                spreadRadius: 0,
+                blurRadius: isDesktop ? 12 : 6,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          child: TextField(
+            controller: widget.controller,
+            focusNode: _focusNode,
+            style: TextStyle(
+              fontSize: isDesktop ? 16 : 14,
             ),
-          ],
-        ),
-        child: TextField(
-          controller: widget.controller,
-          focusNode: _focusNode,
-          decoration: InputDecoration(
-            hintText: 'Search Products...',
-            prefixIcon: GestureDetector(
-              onTap: () {
-                _focusNode.requestFocus();
-              },
-              child: const Icon(Icons.search, color: Color.fromARGB(255, 56, 56, 56)),
-            ),
-            suffixIcon: widget.isSearching
-                ? IconButton(
-                    onPressed: widget.onClear,
-                    icon: const Icon(Icons.clear),
-                  )
-                : null,
-            border: InputBorder.none,
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: AppConstants.defaultPadding,
-              vertical: AppConstants.defaultPadding,
+            decoration: InputDecoration(
+              hintText: isDesktop ? 'Search products by name, category...' : 'Search Products...',
+              hintStyle: TextStyle(
+                fontSize: isDesktop ? 16 : 14,
+                color: Colors.grey[600],
+              ),
+              prefixIcon: GestureDetector(
+                onTap: () {
+                  _focusNode.requestFocus();
+                },
+                child: Icon(
+                  Icons.search, 
+                  color: const Color.fromARGB(255, 56, 56, 56),
+                  size: isDesktop ? 24 : 20,
+                ),
+              ),
+              suffixIcon: widget.isSearching
+                  ? IconButton(
+                      onPressed: widget.onClear,
+                      icon: Icon(
+                        Icons.clear,
+                        size: isDesktop ? 24 : 20,
+                      ),
+                      tooltip: 'Clear search',
+                    )
+                  : null,
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: isDesktop ? 20 : AppConstants.defaultPadding,
+                vertical: isDesktop ? 18 : AppConstants.defaultPadding,
+              ),
             ),
           ),
         ),
